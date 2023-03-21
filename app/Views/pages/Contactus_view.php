469068125bae83d5a23673da6cb89642b481b7e6
Alenalex1 <alenalex1045@gmail.com>
2023-03-21 19:56:20 +0530

    authentication and login completed

---
+++ app/Views/pages/Contactus_view.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+	<title>contactus-section</title>
+   <meta charset="UTF-8">
+   <link rel="stylesheet" href="./css/contactus_css.css">
+</head>
+<body>
+<div class="contactus-section">
+    <h1>Contact Us </h1>
+    <p></p>
+    <p> </p>
+  </div>
+  
+  <h2 style="text-align:center">Our Team</h2>
+  <div class="row">
+    <div class="column">
+      <div class="card">
+        <div class="container">
+          <h2>Reception</h2>
+          <p class="title"></p>
+          <p>Reception@example.com</p>
+          <p><button class="button">Contact</button></p>
+        </div>
+      </div>
+    </div>
+  
+    <div class="column">
+      <div class="card">
+        <div class="container">
+          <h2>HELP DESK</h2>
+          <p class="title"></p>
+          <p></p>
+          <p>helpdesk@example.com</p>
+          <p><button class="button">Contact</button></p>
+        </div>
+      </div>
+    </div>
+    <div class="column">
+        <div class="card">
+          <div class="container">
+            <h2>Emergency</h2>
+            <p class="title"></p>
+            <p></p>
+            <p>emergency@example.com</p>
+            <p><button class="button">Contact</button></p>
+          </div>
+        </div>
+      </div>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
